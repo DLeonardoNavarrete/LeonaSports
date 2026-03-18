0173299195029fa568ec577b0f6b544445efe8d6
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-18 09:54:04 -0500

    rutas de diseño

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css?=v1.1'
-import './App.css?=v1.1'
+import './diseño/Index.css?=v1.1'
+import './diseño/App.css?=v1.1'
 import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
