ee1872abb3b246b9074664019df122ccf42b4b13
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-18 10:24:29 -0500

    rutas de diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import OrderForm from './components/OrderForm';
 import OrderItem from './components/OrderItem';
-import './App.css';
+import '.diseño/App.css?=v1.1';
 
 const App = () => {
   const [orders, setOrders] = useState([]);
