d2d91008dcfec174055b07045d0f60b946b3fd58
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-19 14:08:21 -0500

    importar header y estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react';
 import './diseño/App.css';
+import Header from './components/Header';
 
 const App = () => {
+    const [seccion, setSeccion] = useState('home');
+    return (
+        <div className="App">
+            <Header seccion={seccion} />
+            <main>
+                <h2>Bienvenido a Leona Sports</h2>
+                <p>Tu tienda de deportes en línea</p>
+            </main>
+        </div>
+    );
 };
 
 export default App;
