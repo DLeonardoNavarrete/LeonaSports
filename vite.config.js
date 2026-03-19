28876f9710432ade476be85a0b944fa559ae2e23
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-19 14:28:25 -0500

    confi

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { es } from 'zod/locales'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    cssMinify: 'esbuild',
+  },
 })
