d8f455a53555e418da54c100154f5ac309c0e99c
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-19 14:52:08 -0500

    confi tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { es } from 'zod/locales'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/',
+  build: {
+    cssMinify: 'esbuild',
+  }
 })
