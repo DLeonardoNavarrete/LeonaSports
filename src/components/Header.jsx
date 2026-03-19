36a778f80e0bd3c5055faeb7de75b8f1dcea4c52
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-19 14:37:58 -0500

    confi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = ({ seccion }) => {
+const Header = () => {
   return (
     <header className="main-header">
-      <h1 className="nav-container">
       <nav>
-        <Link to="/" className="nav-logo">Leona Sports</Link>
-        <p>Tu tienda de deportes en línea</p>
+        <h1 className="nav-container">
+          <Link to="/" className="nav-logo">Leona Sports</Link>
+          <p>Tu tienda de deportes en línea</p>
+        </h1>
       </nav>
-      </h1>
     </header>
   );
 };
