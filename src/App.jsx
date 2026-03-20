c5dd0d878c1758ea5563ad9b1f9ddd8ff9debf16
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-20 15:08:27 -0500

    llamar carrusel

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 // Componentes
 import Header from './components/header/Header';
+import Carrusel from './components/carrusel/carrusel';
 
 // Estilos
 import './diseño/App.css';
@@ -14,6 +15,14 @@ const App = () => {
         <div className="App">
             <Header/>
             <div className="content-App">
+                <Carrusel
+                    imagenes={[
+                        {src: "/images/logo.png", link: "/", pestañaNueva: true}
+                    ]}
+                    tituloSeccion="Bienvenida a Leona Sports"
+                    parrafoSeccion="¡Bienvenidos a Leona Sports! Somos una comunidad apasionada por el deporte, dedicada a compartir noticias, eventos y contenido exclusivo sobre el mundo deportivo. Únete a nosotros para estar al tanto de las últimas novedades y disfrutar de una experiencia única en el mundo del deporte."
+                    idSeccion="bienvenida"
+                />
             </div>
         </div>
     );
