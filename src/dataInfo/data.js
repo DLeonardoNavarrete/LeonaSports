30f6b3fe9f95cf19788d787856a2d08b7098ef4a
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-24 15:23:22 -0500

    estructura codex

--- src/dataInfo/data.js
+++ src/dataInfo/data.js
@@ -1,40 +1,32 @@
-import { id } from "zod/locales";
-
-export const countries= [
-    {
-        id: 1,
-        titulo: "Seleccion Colombia",
-        parrafo: "Camiseta para Hombre y mujer, del mundial 2026",
-        imagenes: [
-            {src: "/images/logo.png", },
-            {src: "/images/logo.png", }
-        ]
-    },
-    {
-        id: 2,
-        titulo: "Selecciones",
-        parrafo: "Selecciones Mundialistas",
-        imagenes: [
-            {src: "/images/logo.png", link: "/"}
-        ]
-    }
+export const countries = [
+  {
+    id: 1,
+    titulo: 'Seleccion Colombia',
+    parrafo: 'Camiseta para hombre y mujer del mundial 2026',
+    imagenes: [
+      { src: '/images/logo.png' },
+      { src: '/images/logo.png' },
+    ],
+  },
+  {
+    id: 2,
+    titulo: 'Selecciones',
+    parrafo: 'Selecciones mundialistas',
+    imagenes: [{ src: '/images/logo.png', link: '/' }],
+  },
 ];
 
-export const detCountries= [
-    {
-        id: 1,
-        titulo: "Ligas de Europa",
-        parrafo: "detalle de las ligas",
-        imagenes: [
-            {src: "/images/logo.png", link: "/"}
-        ]
-    },
-    {
-        id: 2,
-        titulo: "Ligas de Sudamerica",
-        parrafo: "detalle de las ligas de sudamérica",
-        imagenes: [
-            {src: "/images/images_coorp", }
-        ]
-    }
-]
+export const detCountries = [
+  {
+    id: 1,
+    titulo: 'Ligas de Europa',
+    parrafo: 'Detalle de las ligas',
+    imagenes: [{ src: '/images/logo.png', link: '/' }],
+  },
+  {
+    id: 2,
+    titulo: 'Ligas de Sudamerica',
+    parrafo: 'Detalle de las ligas de Sudamerica',
+    imagenes: [{ src: '/images/logo.png' }],
+  },
+];
