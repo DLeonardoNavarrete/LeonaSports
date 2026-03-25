a981cfaa08db4357eeb89104fe0fef59fecdc025
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-25 09:23:28 -0500

    declaración de variables en fuentes tailwind.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        'leona-gold': '#dcb64e',   // Un dorado elegante para detalles
-        'leona-dark': '#1A1A1A',   // Negro mate para el fondo premium
-        'leona-gray': '#d9d9d9',   // Gris claro para contraste
-      },
       fontFamily: {
-        'bebas': ['"Bebas Neue"', 'sans-serif'],
-        'montserrat': ['Montserrat', 'sans-serif'],
+        bebas: ['"Bebas Neue"', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
       },
     },
   },
